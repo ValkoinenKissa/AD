625d3ec4e104c1f3588c33378c210234cf16a2cb
Alberto Bollo <albertobollo7@protonmail.com>
2025-11-29 21:45:14 +0100

    fix: fixed Scanner singleton implementation

--- Actividad_trimestral_1/src/main/java/utils/ScannerUtil.java
+++ Actividad_trimestral_1/src/main/java/utils/ScannerUtil.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class ScannerUtil {
     private static Scanner sc;
 
+    private ScannerUtil(){
+
+    }
+
     public static Scanner getScanner(){
-        if (sc==null){
-            createScanner();
+        if (sc == null){
+            sc = new Scanner(System.in);
         }
 
         return sc;
-    }
-
-    private static void createScanner() {
-
-        new Scanner(System.in);
     }
 }
